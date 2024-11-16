35ddda4f5df420f75f441d716f68e4c4162a956b
Bianna Chen <bchen39@binghamton.edu>
2024-11-16 16:08:50 -0500

    starting map class

---
+++ template_final_project-master/map.py
@@ -0,0 +1,10 @@
+import pygame
+class Map():
+    def __init__(self):
+        self.fullmap = False
+        self.kanasHouse = False
+        self.supermarket = False
+        self.forest = False
+        self.butchery = False
+        self.bubbysHouse = False
+        
