e40faaca78455e6c339bddd599a76abb8c0ba2cb
Bianna Chen <bchen39@binghamton.edu>
2024-11-26 18:01:03 -0500

    a little more

--- template_final_project-master/src/task.py
+++ template_final_project-master/src/task.py
@@ -1,3 +1,3 @@
 class Task():
     def __init__(self):
-        
+        pass
--- template_final_project-master/src/view.py
+++ template_final_project-master/src/view.py
@@ -6,11 +6,10 @@ class View:
         self.screen = pygame.display.set_mode()
         self.background = pygame.image.load("template_final_project-master/assets/map.jpeg")
         self.background = pygame.transform.scale(self.background, self.screen.get_size())
-        #self.screen.blit() = None Draws one surface on top of another surface
         
     def render_main(self):
         self.screen.fill((0,0,0))
-        self.screen.blit(self.background, (0,0))
+        self.screen.blit(self.background, (0,0)) #Draws one surface on top of another surface
       
         
         pygame.display.flip() #doesnn't display anything until the program internally loads everything (displays eveyrthing at once). Call it only once every frame
@@ -18,7 +17,7 @@ class View:
     def events(self):
         return pygame.event.get() #Makes events, need it for the for loop to check if player made a keyboard commad\
     
-    def quit(self):
+    def quit(self): #code to quit the program. Command c on the terminal works too I think
         pygame.quit()
         sys.exit()
     
