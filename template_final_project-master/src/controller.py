378300b02640f74c47c7a03407e099c547bfdfef
Bianna Chen <bchen39@binghamton.edu>
2024-12-04 19:03:07 -0500

    prof helped us (im crying AHHH)

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from src.home import Home
+from src.food import Food
 from src.kana import Kana
 
 class Controller:
@@ -8,12 +8,12 @@ class Controller:
   def __init__(self):
     #setup pygame data
     pygame.init()
-    self.screen = pygame.display.set_mode()
+    self.screen = pygame.display.set_mode((400,400))
     self.background = pygame.image.load("template_final_project-master/assets/map.jpeg")
     self.background = pygame.transform.scale(self.background, self.screen.get_size())
     
     #textbox
-    self.textbox = pygame.display.set_mode()
+  
     # Fill background
     self.box = pygame.Surface(self.textbox.get_size())
     self.box = self.textbox.convert()
@@ -27,10 +27,11 @@ class Controller:
     textpos.centerx = self.box.get_rect().centerx
     self.box.blit(text, textpos)
 
+
+    #Put food here
+    self.width, self.height = self.screen.get_size()
+    self.friedrice = Food("template_final_project-master/assets/friedrice.png", self.width,self.height)
     
-    
-  
-    self.myhome = Home()
     self.kana = Kana()
     self.show_player = False
     #self.kana = Kana() Put image in parathese
@@ -44,15 +45,19 @@ class Controller:
   def quit(self): #code to quit the program. Command c on the terminal works too I think
         pygame.quit()
         sys.exit()
+  
+
       
   def mainloop(self):
+    print(self.x,self.y)
     #select state loop
     run = True
     while run:
       # Blit everything to the screen
-      if self.start_game == True:
-        self.textbox.blit(self.box, (0, 0))
-        pygame.display.flip()
+      print("here 1")
+      # if self.start_game == True:
+      #   self.textbox.blit(self.box, (0, 0))
+      #   pygame.display.flip()
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           print("quit")
@@ -74,11 +79,13 @@ class Controller:
             
           if event.key == pygame.K_RIGHT:
             self.kana.move_right()
-      
+      print("here 2")
       self.render_main() 
       if self.show_player == True:  
         self.screen.blit(self.kana.img, self.kana.rect)
+        self.screen.blit(self.friedrice.img, self.friedrice.rect)
         
+      print("here 3")
       pygame.display.flip()
 
 
