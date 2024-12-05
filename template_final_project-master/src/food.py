be1b7266a6568b3cf7ede00624417186ed25b101
Your Name <you@example.com>
2024-12-05 17:34:06 -0500

    class docs

--- template_final_project-master/src/food.py
+++ template_final_project-master/src/food.py
@@ -6,9 +6,10 @@ import random
 class Food(pygame.sprite.Sprite):
     def __init__(self, image, screensize_x, screensize_y):
         super().__init__()
-        '''
-        initializes food image
-        '''
+        """
+        initializes food objects
+        args: self, (string) image associated with object, (int) random x coord based on screen sixe, (int) random y coord based on screen size
+        """
         self.image_show = image
         self.img= pygame.image.load(self.image_show).convert_alpha()
         self.img = pygame.transform.scale(self.img, (60,60))
@@ -17,9 +18,18 @@ class Food(pygame.sprite.Sprite):
         self.rect.y = random.randint(0,screensize_y - self.rect.height)
         
     def events(self):
+        """
+        makes and checks user events
+        args: self
+        return: pygame.event.get()
+        """
         return pygame.event.get() #Makes events, need it for the for loop to check if player made a keyboard commad\
     
     def quit(self): #code to quit the program. Command c on the terminal works too I think
+        """
+        quits game
+        args: self
+        """
         pygame.quit()
         sys.exit()
         
--- template_final_project-master/src/kana.py
+++ template_final_project-master/src/kana.py
@@ -4,9 +4,10 @@ from src.food import Food
 class Kana(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        '''
-        initializes kana player image
-        '''
+        """
+        initializes kana object
+        args: self
+        """
         self.img= pygame.image.load("template_final_project-master/assets/characters/kanaclose.png").convert_alpha()
         self.img = pygame.transform.scale(self.img, (60,60))
         self.rect = self.img.get_rect()
@@ -16,13 +17,29 @@ class Kana(pygame.sprite.Sprite):
     
 
     def move_up(self):
+        """
+        moves kana up
+        args: self
+        """
         self.rect.y -= self.speed
 
     def move_down(self):
+        """
+        moves kana down
+        args: self
+        """
         self.rect.y += self.speed
 
     def move_right(self):
+        """
+        moves kana right
+        args: self
+        """
         self.rect.x += self.speed
 
     def move_left(self):
+        """
+        moves kana left
+        args: self
+        """
         self.rect.x -= self.speed
